c1b0fab38d6270ba7eb345ae98d2fae8f900b29a
unknown <ahmed.elmagdy51@gmail.com>
2025-06-26 21:14:27 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout>
+    <Header />
+    <v-main class="bg-grey-lighten-3" style="min-height: 100vh">
+      <NuxtPage />
+    </v-main>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    lang: "ar",
+    dir: "rtl",
+  },
+});
+</script>
+
+<style>
+body {
+  font-family: "Cairo", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
---
+++ pages/add-product.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>إضافة منتج</h2>
+  </div>
+</template>
